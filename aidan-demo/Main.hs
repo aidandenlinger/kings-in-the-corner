eb8ffb4f6a7ef46ed9191a4a391d92d1592858e3
Aidan Denlinger <47428697+aidandenlinger@users.noreply.github.com>
2022-11-10 16:08:59 -0800

    feat: render a card

--- aidan-demo/Main.hs
+++ aidan-demo/Main.hs
@@ -1,9 +1,58 @@
 module Main (main) where
 
 import Brick
+import Brick.Widgets.Border (border)
+import Brick.Widgets.Border.Style (unicodeRounded)
+import Graphics.Vty.Attributes (defAttr, Attr)
+import Graphics.Vty.Attributes.Color (blue)
+import System.Exit (exitSuccess)
+import Brick.Widgets.Center (center)
 
-ui :: Widget ()
-ui = str "this is aidan's demo :0"
+-- width, height of cards
+cardSize :: (Int, Int)
+cardSize = (4,3)
 
+-- stolen from
+-- <https://github.com/ambuc/solitaire/blob/0ada6e445c85f2f61c15081be49a99df6e272d29/src/Render.hs#L18>
+-- given a widget, sets the border to be rounded :)
+cardStyle :: Widget n -> Widget n
+cardStyle = withBorderStyle unicodeRounded . border
+
+-- takes in card text, centers it, makes it card size
+cardWidget :: String -> Widget ()
+cardWidget text = setAvailableSize cardSize $ center $ str text
+
+-- attributes that widgets can use
+attrs :: [(AttrName, Attr)]
+attrs = [(attrName "selected_card", fg blue)]
+
+-- marks a card as selected
+isSelected :: Widget n -> Widget n
+isSelected = withAttr (attrName "selected_card")
+
+-- start point of this executable
 main :: IO ()
-main = simpleMain ui
+main = do
+  let app =
+        App
+          { -- given a state, return list of widgets to draw. in this case,
+            -- ignore state and just draw card, with label as selected
+            appDraw = const [isSelected $ cardStyle $ cardWidget "5❤️"],
+            -- given state and an event, describe how to change state. this
+            -- helper func in Brick.Main halts the program on any event
+            -- except a resize
+            appHandleEvent = resizeOrQuit,
+            -- returns an EventM that runs at app start, this is a demo,
+            -- there's nothing to do at start, return
+            appStartEvent = return,
+            -- holds attributes that can be applied to elements
+            appAttrMap = const $ attrMap defAttr attrs,
+            -- don't show cursor
+            appChooseCursor = neverShowCursor
+          }
+  -- use defaultMain to start our app
+  -- use [] as our state since this app doesn't store anything
+  -- ignore the final returned state, because this app doesn't store anything
+  _ <- defaultMain app []
+  -- exit once done, don't check anything
+  exitSuccess
