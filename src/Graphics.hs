fe96a8aed0ef2c2b3d7decc3c61b7f446eb2e49d
Aidan Denlinger <47428697+aidandenlinger@users.noreply.github.com>
2022-11-22 21:56:33 -0800

    doc: add more documentation, reorder a bit

--- src/Graphics.hs
+++ src/Graphics.hs
@@ -2,7 +2,7 @@ module Graphics (gameStart) where
 
 import Brick
 import Brick.Widgets.Border (border)
-import Brick.Widgets.Border.Style as BS -- (unicodeRounded, bsVertical)
+import Brick.Widgets.Border.Style as BS
 import Brick.Widgets.Center
 import CardTypes
 import Data.List.Index (modifyAt)
@@ -11,6 +11,8 @@ import Graphics.Vty.Attributes (Attr, defAttr)
 import Graphics.Vty.Attributes.Color (blue, green)
 import System.Exit (exitSuccess)
 
+--- CONSTANTS
+
 -- dimension of cards
 cardWidth :: Int
 cardWidth = 4
@@ -18,6 +20,10 @@ cardWidth = 4
 cardHeight :: Int
 cardHeight = 3
 
+cardSize :: (Int, Int)
+cardSize = (cardWidth, cardHeight)
+
+-- Padding values
 handPadding :: Int
 handPadding = 1
 
@@ -27,27 +33,63 @@ pileHorizPadding = 4
 pileVertPadding :: Int
 pileVertPadding = 0
 
-cardSize :: (Int, Int)
-cardSize = (cardWidth, cardHeight)
+type GameState = (Int, Int, Int)
 
--- stolen from
--- <https://github.com/ambuc/solitaire/blob/0ada6e445c85f2f61c15081be49a99df6e272d29/src/Render.hs#L18>
--- given a widget, sets the border to be rounded :)
-cardStyle :: Widget n -> Widget n
-cardStyle = withBorderStyle BS.unicodeRounded . border
+--- MAIN DRAW FUNCTION
 
-italicStyle :: Widget n -> Widget n
-italicStyle = withBorderStyle custom . border
+-- Given the GameState, return the widget to draw
+draw :: GameState -> Widget ()
+draw (sel, place, _) = vBox [topPiles, playerHand]
   where
-    custom =
-      BS.BorderStyle
-        { BS.bsCornerTL = '╒',
-          BS.bsCornerTR = '╕',
-          BS.bsCornerBR = '╛',
-          BS.bsCornerBL = '╘',
-          BS.bsHorizontal = '─',
-          BS.bsVertical = '/'
-        }
+    -- TODO: Don't use hardcoded values, get piles from gamestate
+    topPiles = createTopPiles place [topPile, rightPile, bottomPile, leftPile]
+    playerHand = createPlayerHand sel playerCards
+
+--- PILES
+
+-- Given the top, right, bottom, and left pile, along with the currently
+-- selected pile, return the widget for the piles of the board.
+-- This expects a list of four elements - top pile, right pile, bottom pile,
+-- left pile. TODO: better datatype in the future, order shouldn't matter
+-- TODO: Don't use an Int to signify what pile we're pointing at, use an enum
+createTopPiles :: Int -> [[Card]] -> Widget ()
+createTopPiles place piles
+  | length piles == 4 =
+      vBox $
+        map
+          ( padTopBottom pileVertPadding
+              . hCenter
+              . hBox
+              . map (padLeftRight pileHorizPadding)
+          )
+          -- TODO: fill corner and middle decks with real values
+          [ [cardWidget "top\nleft", topWidget, cardWidget "top\nrigh\nt"],
+            [leftWidget, cardWidget "deck", rightWidget],
+            [cardWidget "bot\nleft", bottomWidget, cardWidget "bot\nrigh\nt"]
+          ]
+  where
+    [topWidget, rightWidget, bottomWidget, leftWidget] =
+      modifyAt place placeCard $
+        map pileToOverlap piles
+createTopPiles _ _ = error "hardcoded for four piles for now :)"
+
+--- Given a pile, return a widget showing the top and bottom card of the pile
+pileToOverlap :: [Card] -> Widget ()
+pileToOverlap pile = vBox [cardWidgetHalf bottomCard, cardWidget topCard]
+  where
+    bottomCard = show (last pile)
+    topCard = show (head pile)
+
+-- Given an index of selected card and list of cards, return a widget of the
+-- player's hand.
+createPlayerHand :: Int -> [Card] -> Widget ()
+createPlayerHand sel hand =
+  center $
+    hBox $
+      modifyAt sel isSelected $
+        map (padLeftRight handPadding . cardWidget . show) hand
+
+--- CARDS AND STYLE
 
 -- Don't use directly, used by cardWidget and cardWidgetHalf
 createCard :: (Widget n1 -> Widget n2) -> String -> Widget n2
@@ -68,6 +110,25 @@ cardWidgetHalf n =
   where
     halfCardHeight = max 2 (cardHeight `div` 2)
 
+-- stolen from
+-- <https://github.com/ambuc/solitaire/blob/0ada6e445c85f2f61c15081be49a99df6e272d29/src/Render.hs#L18>
+-- given a widget, sets the border to be rounded :)
+cardStyle :: Widget n -> Widget n
+cardStyle = withBorderStyle BS.unicodeRounded . border
+
+italicStyle :: Widget n -> Widget n
+italicStyle = withBorderStyle custom . border
+  where
+    custom =
+      BS.BorderStyle
+        { BS.bsCornerTL = '╒',
+          BS.bsCornerTR = '╕',
+          BS.bsCornerBR = '╛',
+          BS.bsCornerBL = '╘',
+          BS.bsHorizontal = '─',
+          BS.bsVertical = '/'
+        }
+
 -- attributes that widgets can use
 attrs :: [(AttrName, Attr)]
 attrs = [(attrName "selected_card", fg blue), (attrName "place_card", fg green)]
@@ -79,24 +140,6 @@ isSelected = withAttr (attrName "selected_card")
 -- where to place card
 placeCard :: Widget n -> Widget n
 placeCard = withAttr (attrName "place_card")
-
-type GameState = (Int, Int, Int)
-
-handleEvent :: GameState -> BrickEvent n e -> EventM n (Next GameState)
--- Left and Right move between player cards
-handleEvent (sel, place, numCards) (VtyEvent (EvKey KLeft _)) =
-  continue ((sel - 1) `mod` numCards, place, numCards)
-handleEvent (sel, place, numCards) (VtyEvent (EvKey KRight _)) =
-  continue ((sel + 1) `mod` numCards, place, numCards)
--- Up and Down move between decks
-handleEvent (sel, place, numCards) (VtyEvent (EvKey KUp _)) =
-  continue (sel, (place + 1) `mod` 4, numCards)
-handleEvent (sel, place, numCards) (VtyEvent (EvKey KDown _)) =
-  continue (sel, (place - 1) `mod` 4, numCards)
--- Esc quits game
-handleEvent s (VtyEvent (EvKey KEsc [])) = halt s
--- Everything else does not change state
-handleEvent s _ = continue s
 
 -- Example hands
 playerCards :: [Card]
@@ -114,52 +157,31 @@ rightPile = [Card R6 Club, Card RK Diamond]
 bottomPile :: [Card]
 bottomPile = [Card RK Diamond, Card RJ Diamond]
 
-pileToOverlap :: [Card] -> Widget ()
-pileToOverlap pile = vBox [cardWidgetHalf bottomCard, cardWidget topCard]
-  where
-    bottomCard = show (last pile)
-    topCard = show (head pile)
+--- EVENT HANDLING
 
--- This expects a list of four elements - top pile, right pile, bottom pile,
--- left pile. This should be not hardcoded in the future
-createTopPiles :: Int -> [[Card]] -> Widget ()
-createTopPiles place piles
-  | length piles == 4 =
-      vBox $
-        map
-          (padTopBottom pileVertPadding . hCenter . hBox . map (padLeftRight pileHorizPadding))
-          [ [cardWidget "top\nleft", topWidget, cardWidget "top\nrigh\nt"],
-            [leftWidget, cardWidget "deck", rightWidget],
-            [cardWidget "bot\nleft", bottomWidget, cardWidget "bot\nrigh\nt"]
-          ]
-  where
-    [topWidget, rightWidget, bottomWidget, leftWidget] =
-      modifyAt place placeCard $
-        map pileToOverlap piles
-createTopPiles _ _ = error "hardcoded for four piles for now :)"
+handleEvent :: GameState -> BrickEvent n e -> EventM n (Next GameState)
+-- Left and Right move between player cards
+handleEvent (sel, place, numCards) (VtyEvent (EvKey KLeft _)) =
+  continue ((sel - 1) `mod` numCards, place, numCards)
+handleEvent (sel, place, numCards) (VtyEvent (EvKey KRight _)) =
+  continue ((sel + 1) `mod` numCards, place, numCards)
+-- Up and Down move between decks
+handleEvent (sel, place, numCards) (VtyEvent (EvKey KUp _)) =
+  continue (sel, (place + 1) `mod` 4, numCards)
+handleEvent (sel, place, numCards) (VtyEvent (EvKey KDown _)) =
+  continue (sel, (place - 1) `mod` 4, numCards)
+-- Esc quits game
+handleEvent s (VtyEvent (EvKey KEsc [])) = halt s
+-- Everything else does not change state
+handleEvent s _ = continue s
 
-createPlayerHand :: Int -> [Card] -> Widget ()
-createPlayerHand sel hand =
-  center $
-    hBox $
-      modifyAt sel isSelected $
-        map (padLeftRight handPadding . cardWidget . show) hand
-
-draw :: GameState -> Widget ()
-draw (sel, place, _) = vBox [topPiles, playerHand]
-  where
-    topPiles = createTopPiles place [topPile, rightPile, bottomPile, leftPile]
-    playerHand = createPlayerHand sel playerCards
+--- GAME START
 
 gameStart :: IO ()
 gameStart = do
   let app =
         App
-          { -- given a state, return list of widgets to draw. in this case,
-            -- state is the index of the card selected and the number of cards.
-            -- make the playerHand a series of widgets, make the selected card
-            -- selected, then use a fold to combine them all horizontally, and
-            -- finally vertically append some text that states what is selected
+          { -- given a state, return list of widgets to draw.
             appDraw = \s -> [draw s],
             -- given state and an event, describe how to change state. the app
             -- is then redrawn
@@ -167,15 +189,14 @@ gameStart = do
             -- returns an EventM that runs at app start, this is a demo,
             -- there's nothing to do at start, return
             appStartEvent = return,
-            -- holds attributes that can be applied to elements, ex
-            -- selected_card
+            -- holds attributes that can be applied to elements
             appAttrMap = const $ attrMap defAttr attrs,
             -- don't show cursor
             appChooseCursor = neverShowCursor
           }
   -- use defaultMain to start our app
-  -- use [] as our state since this app doesn't store anything
-  -- ignore the final returned state, because this app doesn't store anything
+  -- ignore the final returned state for now, because this app doesn't store
+  -- anything
   _ <- defaultMain app (0, 0, length playerCards)
   -- exit once done, don't check anything
   exitSuccess
