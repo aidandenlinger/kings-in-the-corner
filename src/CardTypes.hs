bfe3d9317697d17c91232f0888a315e936e6394c
Mayank Sharan <mayanksharan96@gmail.com>
2022-11-27 17:28:35 -0800

    msharan/game-play: Game state initialization added

--- src/CardTypes.hs
+++ src/CardTypes.hs
@@ -99,6 +99,8 @@ data GSt = GSt { _field   :: Field            -- Current state of decks
                , _seed    :: R.StdGen         -- A random seed to be passed thru
                , _history :: [(Field, Int)]   -- List of previous fields and corresponding player ids
                , _toplay  :: Int              -- Player id of the one with next move
+               , _selcds  :: Maybe DCard      -- A card if selected in the graphics
+               , _selpile :: Maybe Pile       -- A pile if selected in the graphics
                } deriving (Show)
 
 -- DISPLAY TYPES ---------------------------------------------------------------
--- src/Utils.hs
+++ src/Utils.hs
@@ -1,5 +1,11 @@
+{-# LANGUAGE TemplateHaskell #-}
+
 module Utils
-  ( assignColor
+  ( assignColor,
+    initialDeal,
+    genPlPile,
+    initGSt,
+    getPHands
   ) where
 
 import Data.List.Split (splitPlaces)
@@ -13,14 +19,19 @@ import CardTypes
 -------------------------------------------------------------------------------
 -- Convert data types from CardTypes to Lenses for easy access
 
--- makeLenses ''DCard
--- makeLenses ''Pile
--- makeLenses ''Field
--- makeLenses ''GSt
+makeLenses ''DCard
+makeLenses ''Pile
+makeLenses ''Field
+makeLenses ''GSt
 
 ------------------------------------------------------------------------------- 
 
 -- Base code borrowed from https://github.com/ambuc/solitaire
+
+-- Initialize any constants that might be needed
+
+nCardsInHand :: Int
+nCardsInHand = 7
 
 -- assigns colors to suits
 
@@ -38,4 +49,95 @@ allSuits :: [Suit]
 allSuits = [minBound .. maxBound] :: [Suit] -- list of all suits
 
 -- the default deal is a sorted list of cards to be shuffled on game state initialization
+initialDeal :: [Card]
 initialDeal = [ Card r s | r <- allRanks, s <- allSuits ]
+
+-- Generate a pile for n players from the given deal of cards
+-- This is done by taking the top 7 cards and recursively calling the function
+-- Assume there are enough cards in the deck
+
+genPlPile :: Int -> [Card] -> [Pile]
+genPlPile nPlayers deal 
+  | nPlayers <= 0 = []
+  | otherwise     = initHand : genPlPile (nPlayers - 1) (drop nCardsInHand deal)
+  where
+    initHand      = Pile { _cards    = [ DCard { _card    = c,
+                                                  _facedir = FaceDown 
+                                                } 
+                                          | c <- take nCardsInHand deal
+                                        ],
+                           _display  = Stacked,
+                           _rankBias = Nothing,
+                           _suitBias = Nothing,
+                           _pileType = PlayerP
+                         }     
+
+genCenCorPiles :: [Card] -> ([Pile], [Pile]) -> ([Pile], [Pile])
+genCenCorPiles deal@((Card RK _):cs) (cenPiles, corPiles)
+  | length cenPiles == 4  = (cenPiles, corPiles)
+  | otherwise             = genCenCorPiles cs (cenPiles, initCorPile:corPiles)
+  where
+    initCorPile     = Pile { _cards    = [ DCard { _card    = ctop,
+                                                  _facedir = FaceUp 
+                                                } 
+                                          | ctop <- take 1 deal
+                                        ],
+                             _display  = Stacked,
+                             _rankBias = Nothing,
+                             _suitBias = Nothing,
+                             _pileType = CornerP
+                           }
+
+genCenCorPiles (c:cs) (cenPiles, corPiles)
+  | length cenPiles == 4  = (cenPiles, corPiles)
+  | otherwise             = genCenCorPiles cs (initCenPile:cenPiles, corPiles)
+  where
+    initCenPile     = Pile { _cards    = [ DCard { _card    = c,
+                                                  _facedir = FaceUp 
+                                                } 
+                                        ],
+                             _display  = Stacked,
+                             _rankBias = Nothing,
+                             _suitBias = Nothing,
+                             _pileType = CenterP
+                           }
+
+-- Adding to complete patter matching
+
+genCenCorPiles [] (cenPiles, corPiles) = (cenPiles, corPiles)
+
+-- take a random generator and initialize a game state
+
+initGSt :: Int -> R.StdGen -> GSt
+initGSt nPlayers seedval = GSt { _field   = fieldval,
+                                 _seed    = seedval,
+                                 _history = [],
+                                 _toplay  = 0,
+                                 _selcds  = Nothing,
+                                 _selpile = Nothing
+                               }
+  where
+    deal      = R.shuffle' initialDeal 52 seedval -- Shuffle the initial deal
+    fieldval  = Field { _draw = drawval,
+                        _center = centerval, 
+                        _corner = cornerval,
+                        _phands = phandsval
+                      }
+    phandsval = genPlPile nPlayers deal
+    centerval = fst ccpiles
+    cornerval = snd ccpiles
+    ccpiles   = genCenCorPiles (drop (nPlayers * nCardsInHand) deal) ([], [])
+    drawval   = Pile { _cards    = [ DCard { _card    = c,
+                                            _facedir = FaceUp 
+                                          } 
+                                    | c <- drop ((nPlayers * nCardsInHand) + length centerval + length cornerval) deal
+                                  ],
+                       _display  = Stacked,
+                       _rankBias = Nothing,
+                       _suitBias = Nothing,
+                       _pileType = DrawP
+                     }
+
+
+getPHands :: GSt -> [Pile]
+getPHands gameState = gameState ^. field . phands
